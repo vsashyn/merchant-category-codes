8e8e5c5b7592b62b29eadf67af715d0d76dc0353
Vitalii Sashyn <vitaliis@wix.com>
2021-10-11 13:57:33 +0300

    Add package.json

---
+++ index.js
@@ -0,0 +1,20 @@
+const mcc_with_groups = require('./with_groups/mcc.json')
+const mcc_with_groups_en = require('./with_groups/mcc-en.json')
+const mcc_with_groups_ru = require('./with_groups/mcc-ru.json')
+const mcc_with_groups_uk = require('./with_groups/mcc-uk.json')
+
+const mcc_without_groups = require('./without_groups/mcc.json')
+const mcc_without_groups_en = require('./without_groups/mcc-en.json')
+const mcc_without_groups_ru = require('./without_groups/mcc-ru.json')
+const mcc_without_groups_uk = require('./without_groups/mcc-uk.json')
+
+export {
+    mcc_with_groups,
+    mcc_with_groups_en,
+    mcc_with_groups_ru,
+    mcc_with_groups_uk,
+    mcc_without_groups,
+    mcc_without_groups_en,
+    mcc_without_groups_ru,
+    mcc_without_groups_uk,
+}
